18bd63a06e3e135388ec5d056a3db39dd372353e
Tom Mahy <tommahy11@gmail.com>
2023-10-25 16:12:56 +0100

    Minor changes

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 async function getWeather(location){
     const weatherApiKey = `860cab3f0e2041ceb07160613231308`;
 
-    const currentWeather = await getCurrentWeather(location);
     const forecastWeather = await getForecastWeather(location);
-
     
-    console.log(currentWeather, forecastWeather);
-}
+    // console.log(forecastWeather.current, forecastWeather.forecast);
+    // console.log(forecastWeather.current.feelslike_c, forecastWeather.current.temp_c, forecastWeather.current.condition.text);
+    const weather = {
+        current: forecastWeather.current,
+        current_temp: forecastWeather.current.temp_c,
+        current_cond_desc: forecastWeather.current.condition.text,
+        // current_cond_icon: forecastWeather.current.condition.icon, 
 
-async function getCurrentWeather(location){
-    try{
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=860cab3f0e2041ceb07160613231308&q=${location}`);
-    const currentWeatherData = await response.json();
-    return currentWeatherData;
+        forecast_array: forecastWeather.forecast.forecastday
     }
-    catch(error){
-        return error;
-    }
+
+    console.log(weather.current, weather.current_temp, weather.forecast_array);
 }
 
 async function getForecastWeather(location){
     try{
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=860cab3f0e2041ceb07160613231308&q=${location}`);
-    const currentWeatherData = await response.json();
-    return currentWeatherData;
+    //3 day forecast only as free plan on weatherapi.com
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=860cab3f0e2041ceb07160613231308&q=${location}&days=3`);
+    const forecastWeatherData = await response.json();
+    return forecastWeatherData;
     }
     catch(error){
         return error;
